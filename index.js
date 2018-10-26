d1c79fbf8f753138b7d58c12a374a18b68f7f661
Laurent Savaëte <laurent@where.tf>
2018-10-26 16:02:42 +0100

    Add polyfill to allow running on android in release mode

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 /** @format */
 
+// import polyfills to prevent weird errors without remote debugging
+// https://github.com/facebook/react-native/issues/20902#issuecomment-431177779
+import '@babel/polyfill';
+
 import {AppRegistry} from 'react-native';
 import Main from './src/shared/Main.js';
 import {name as appName} from './app.json';
