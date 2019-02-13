2e92318687828da451e24cd84e5670933e6c57a6
Laurent Savaete <laurent@where.tf>
2019-02-13 14:34:52 +0000

    Upgrade sentry plugin to 0.41.1

--- ios/mapswipe/AppDelegate.m
+++ ios/mapswipe/AppDelegate.m
@@ -30,8 +30,7 @@ RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"mapswipe"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-
-  [RNSentry installWithRootView:rootView];
+[RNSentry installWithRootView:rootView];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
