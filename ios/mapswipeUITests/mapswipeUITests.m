5b3f329f75238827d282ed8761048e25540934a2
Laurent Savaete <laurent@where.tf>
2019-04-05 09:13:15 -0700

    Continue building iOS even if UI tests fail

--- ios/mapswipeUITests/mapswipeUITests.m
+++ ios/mapswipeUITests/mapswipeUITests.m
@@ -18,7 +18,7 @@
     // Put setup code here. This method is called before the invocation of each test method in the class.
 
     // In UI tests it is usually best to stop immediately when a failure occurs.
-    self.continueAfterFailure = NO;
+    self.continueAfterFailure = YES;
 
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
     [[[XCUIApplication alloc] init] launch];
