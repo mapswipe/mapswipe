6fc4bb0380fcd571466d57fc8857c220ab81b920
Laurent Savaete <laurent@where.tf>
2019-05-24 14:44:28 +0100

    #42 Use soumak77/firebase-mock

--- __mocks__/react-native-firebase.js
+++ __mocks__/react-native-firebase.js
@@ -1,101 +1,15 @@
-// adapted from https://stackoverflow.com/a/49708199/1138710
-'use strict'
+import firebasemock from 'firebase-mock';
 
-export class Database {
-  ref = (path) => {
-    if (!this[path]) {
-      this[path] = new Reference(path)
-    }
-    return this[path]
-  }
-}
+// using soumak77/firebase-mock
+// we only mock auth and RTDB as we don't need the rest
+const mockauth = new firebasemock.MockAuthentication();
+const mockdatabase = new firebasemock.MockFirebase();
+const mocksdk = new firebasemock.MockFirebaseSdk(
+    path => (path ? mockdatabase.child(path) : mockdatabase),
+    () => mockauth,
+    null,
+    null,
+    null,
+);
 
-export class Reference {
-  constructor(path) {
-    this.path = path
-    this.snap = { val: () => this._val()}
-    this.data = null
-  }
-
-  _val = jest.fn(() => {
-    return this.data
-  })
-
-  once = jest.fn((param, callback) => {
-    const promise = new Promise ((resolve, reject) => {
-      if (callback) {
-        callback(this.snap)
-        resolve()
-      } else {
-        resolve(this.snap)
-      }
-    })
-    RNFirebase.promises.push(promise)
-    return promise
-  })
-
-  on = jest.fn((param, callback) => {
-    const promise = new Promise ((resolve, reject) => {
-      if (callback) {
-        callback(this.snap)
-        resolve()
-      } else {
-        resolve(this.snap)
-      }
-    })
-    RNFirebase.promises.push(promise)
-    return promise
-  })
-
-  off = jest.fn((param, callback) => {
-    const promise = Promise.resolve()
-    RNFirebase.promises.push(promise)
-    return promise
-  })
-
-  update = jest.fn((data) => {
-    const promise = Promise.resolve()
-    RNFirebase.promises.push(promise)
-    return promise
-  })
-
-  remove = jest.fn(() => {
-    const promise = Promise.resolve()
-    RNFirebase.promises.push(promise)
-    return promise
-  })
-}
-
-export class MockFirebase {
-  constructor() {
-    this.database = () => {
-      if (!this.databaseInstance) {
-        this.databaseInstance = new Database()
-      }
-      return this.databaseInstance
-    }
-  }
-}
-
-export default class RNFirebase {
-  static initializeApp() {
-    RNFirebase.firebase = new MockFirebase()
-    RNFirebase.promises = []
-    return RNFirebase.firebase
-  }
-
-  static SDK_VERSION = '5.r.2';
-
-  static reset() {
-    RNFirebase.promises = []
-    RNFirebase.firebase.databaseInstance = null
-  }
-
-  static waitForPromises() {
-    return Promise.all(RNFirebase.promises)
-  }
-
-  static analytics () {}
-
-  static app () {}
-}
+export default mocksdk;
