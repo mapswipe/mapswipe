mapswipe/mapswipe

commit b63ff75929d8b3b82b45985624796d49aba73fca

#42 Fix broken height calculation
(File was missing from pushed commits)

src/shared/views/Mapper/IndividualCard.js
@@ -17,7 +17,7 @@ const GLOBAL = require('../../Globals');
 const styles = StyleSheet.create({
     slide: {
         width: (GLOBAL.SCREEN_WIDTH),
-        height: (GLOBAL.SCREEN_HEIGHT * GLOBAL.TILE_VIEW_HEIGHT),
+        height: (GLOBAL.TILE_VIEW_HEIGHT),
         flex: 1,
         borderWidth: 0,
         backgroundColor: '#212121',

src/shared/Globals.js
@@ -35,7 +35,7 @@ module.exports = {
         DARKGRAY: '#999',
     },
     TASKS_PROCESSING: 0,
-    TILE_VIEW_HEIGHT: (screenHeight - 40 - 30) / screenHeight, // 40 is top bar, 30 is bottom bar
+    TILE_VIEW_HEIGHT: (screenHeight - 40 - 30), // 40 is top bar, 30 is bottom bar
     TILES_PER_VIEW_X: 2,
     TILES_PER_VIEW_Y: 3,
     TILE_VIEW_WIDTH: 1,

src/shared/views/LoadMore.js
@@ -18,7 +18,7 @@ const GLOBAL = require('../Globals');
 const styles = StyleSheet.create({
     congratulationsSlide: {
         width: (GLOBAL.SCREEN_WIDTH),
-        height: (GLOBAL.SCREEN_HEIGHT * GLOBAL.TILE_VIEW_HEIGHT),
+        height: (GLOBAL.TILE_VIEW_HEIGHT),
         borderWidth: 0,
         backgroundColor: '#212121',
         justifyContent: 'center',

src/shared/views/LoadingIcon.js
@@ -25,8 +25,6 @@ type State = {
 };
 
 export default class LoadingComponent extends React.Component<{}, State> {
-    nextOffset: number = 2;
-
     constructor(props: {}) {
         super(props);
         this.state = {

src/shared/views/Mapper/Tile.js
@@ -19,7 +19,7 @@ import type {
 
 const GLOBAL = require('../../Globals');
 
-const tileHeight = GLOBAL.SCREEN_HEIGHT * GLOBAL.TILE_VIEW_HEIGHT * (1 / GLOBAL.TILES_PER_VIEW_Y);
+const tileHeight = GLOBAL.TILE_VIEW_HEIGHT * (1 / GLOBAL.TILES_PER_VIEW_Y);
 const tileWidth = GLOBAL.SCREEN_WIDTH * (1 / GLOBAL.TILES_PER_VIEW_X);
 
 const styles = StyleSheet.create({

commit d7dd087e4476f6389516f80ab9031f7f28a38725

Fix swiping gesture in BuiltArea screen
Broken by b63ff75929d8b

src/shared/views/Mapper/IndividualCard.js
@@ -92,7 +92,6 @@ class _IndividualCard extends React.Component<ICProps, ICState> {
             onMoveShouldSetPanResponder: this.handleMoveShouldSetPanResponder,
             onMoveShouldSetPanResponderCapture: this.handleMoveShouldSetPanResponderCapture,
             onPanResponderGrant: this.handlePanResponderGrant,
-            onPanResponderMove: this.handlePanResponderMove,
             onPanResponderRelease: this.handlePanResponderEnd,
             onPanResponderTerminate: this.handlePanResponderTerminate,
         });
@@ -130,17 +129,11 @@ class _IndividualCard extends React.Component<ICProps, ICState> {
         this.setState({ showSwipeHelp: true });
     };
 
-    handlePanResponderMove = (event: PressEvent, gestureState: GestureState) => {
-        // called on each frame while the user's finger is swiping
-        console.log('ResponderMove', gestureState.dx, gestureState.dy);
-    };
-
     handlePanResponderEnd = (event: PressEvent, gestureState: GestureState) => {
         // swipe completed, decide what to do
         const { card, onToggleTile } = this.props;
-        console.log('ResponderEnd', gestureState.dx, gestureState.dy);
         this.setState({ showSwipeHelp: false });
-        if (gestureState.dy > GLOBAL.TILE_VIEW_HEIGHT * 1.5) {
+        if (gestureState.dy > GLOBAL.TILE_VIEW_HEIGHT * 0.5) {
             card.tileRows.forEach((row) => {
                 row.tiles.forEach((tile) => {
                     onToggleTile({
@@ -148,8 +141,6 @@ class _IndividualCard extends React.Component<ICProps, ICState> {
                         resultId: tile.taskId,
                         result: 3,
                         projectId: tile.projectId,
-                        user: GLOBAL.DB.getAuth().getUser().uid,
-                        timestamp: GLOBAL.DB.getTimestamp(),
                     });
                 });
             });