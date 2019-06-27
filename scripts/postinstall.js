7ead5756733edbe17f1e80e8f937ab45cbe46f6a
Laurent Savaete <laurent@where.tf>
2019-06-28 00:55:18 +0100

    Fix issue with iOS simulator on Xcode > 10.2
    see https://github.com/facebook/react-native/issues/24196

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,34 +1,19 @@
-/**
- *
- * After react-native v0.56.0, `react-native-scrollable-tab-view` case app failed to run. Npm server show below:
-  ```
-  error: bundling failed: SyntaxError: /node_modules/react-native-scrollable-tab-view/SceneComponent.js: A trailing comma is not permitted after the rest element (9:32)
-
-    7 |
-    8 | const SceneComponent = (Props) => {
-  >  9 |   const {shouldUpdated, ...props, } = Props;
-      |                                 ^
-    10 |   return <View {...props}>
-    11 |       <StaticContainer shouldUpdate={shouldUpdated}>
-    12 |         {props.children}
- * ```
- *
- * As the upstream repo in github has not released , post the script to fix it temporary.
- */
-function fixScrollableTabViewTrailingCommaError(){
-  const fs = require('fs');
-  const filePath = 'node_modules/react-native-scrollable-tab-view/SceneComponent.js';
-  fs.readFile(filePath, 'utf8', function (err, data) {
-    if (err) {
-      return console.log(err);
-    }
+function fixXcodeSimulatorSearchString() {
+    const fs = require('fs');
+    const filePath = 'node_modules/react-native/local-cli/runIOS/findMatchingSimulator.js';
+    fs.readFile(filePath, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
 
 
-    const result = data.replace(/const {shouldUpdated, ...props, }/g, 'const {shouldUpdated, ...props}');
-    fs.writeFile(filePath, result, 'utf8', function (err) {
-      if (err) return console.log(err);
+        let result = data.replace(/!version.startsWith('iOS')/g, "!version.includes('iOS')");
+        result = result.replace(/!version.startsWith('tvOS')/g, "!version.includes('tvOS')");
+        fs.writeFile(filePath, result, 'utf8', function (error) {
+            if (error) return console.log(error);
+        });
     });
-  });
 }
 
 
+fixXcodeSimulatorSearchString();
