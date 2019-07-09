mapswipe/mapswipe

commit bfbb4c584b19ee7ffe33a94c84d6e0ff166a7f4b

Refactor main project components (footprint and change detection)

src/shared/common/ProjectLevelScreen.js
@@ -1,0 +1,236 @@
+// @flow
+import * as React from 'react';
+import {
+    BackHandler,
+    StyleSheet,
+    View,
+} from 'react-native';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firebaseConnect, isEmpty, isLoaded } from 'react-redux-firebase';
+import { get } from 'lodash';
+import {
+    cancelGroup,
+    commitGroup,
+    startGroup,
+} from '../actions/index';
+import Header from '../views/Header';
+import BottomProgress from '../views/Mapper/BottomProgress';
+import LoadingIcon from '../views/LoadingIcon';
+import LoadMoreCard from '../views/LoadMore';
+import { getSqKmForZoomLevelPerTile } from '../Database';
+import type {
+    GroupType,
+    NavigationProp,
+    ProjectType,
+    ResultMapType,
+} from '../flow-types';
+import {
+    COLOR_DEEP_BLUE,
+} from '../constants';
+
+const GLOBAL = require('../Globals');
+
+/* eslint-disable global-require */
+
+const styles = StyleSheet.create({
+    mappingContainer: {
+        backgroundColor: COLOR_DEEP_BLUE,
+        height: GLOBAL.SCREEN_HEIGHT,
+        width: GLOBAL.SCREEN_WIDTH,
+    },
+});
+
+type Props = {
+    Component: React.ComponentType<any>,
+    group: { [group_id: string]: GroupType },
+    navigation: NavigationProp,
+    onCancelGroup: {} => void,
+    onStartGroup: {} => void,
+    onSubmitResult: (Object) => void,
+    screenName: string,
+};
+
+type State = {
+    groupCompleted: bool,
+};
+
+class ProjectLevelScreen extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.project = props.navigation.getParam('project');
+        this.state = {
+            groupCompleted: false,
+        };
+    }
+
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
+    }
+
+    componentDidUpdate = (prevProps) => {
+        const { group, onStartGroup } = this.props;
+        if (prevProps.group !== group) {
+            if (isLoaded(group) && !isEmpty(group)) {
+                // the component props are updated when group is received
+                // and then when tasks are received
+                if (group.tasks !== undefined) {
+                    onStartGroup({
+                        groupId: group.groupId,
+                        projectId: group.projectId,
+                        timestamp: GLOBAL.DB.getTimestamp(),
+                    });
+                    this.setState({ groupCompleted: false });
+                    if (this.progress) this.progress.updateProgress(0);
+                }
+            }
+        }
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
+    }
+
+    handleBackPress = () => {
+        this.returnToView();
+        return true;
+    }
+
+    returnToView = () => {
+        const { group, navigation, onCancelGroup } = this.props;
+        // TODO: this will not work with offline preloading of multiple groups
+        // as several groups will be stored in redux, possibly with clashing groupId
+        onCancelGroup({
+            groupId: group.groupId,
+            projectId: group.projectId,
+        });
+        navigation.pop();
+    }
+
+    submitResult = (result, taskId) => {
+        const { group, onSubmitResult } = this.props;
+        const resultObject = {
+            resultId: taskId,
+            result,
+            groupId: group.groupId,
+            projectId: this.project.projectId,
+            timestamp: GLOBAL.DB.getTimestamp(),
+        };
+        onSubmitResult(resultObject);
+    }
+
+    commitCompletedGroup = () => {
+        this.setState({ groupCompleted: true });
+    }
+
+    getContributions = (group, results) => {
+        const contributionsCount = Object.keys(results).length;
+        const addedDistance = group.count * getSqKmForZoomLevelPerTile(19);
+        return { contributionsCount, addedDistance };
+    }
+
+    toNextGroup = () => {
+        const { navigation, screenName } = this.props;
+        navigation.navigate(screenName, { project: this.project });
+    }
+
+    updateProgress = (progress: number) => {
+        if (this.progress) {
+            this.progress.updateProgress(progress);
+        }
+    }
+
+    progress: ?BottomProgress;
+
+    project: ProjectType;
+
+    render = () => {
+        const { Component, group, navigation } = this.props;
+        const { groupCompleted } = this.state;
+        if (!group) {
+            return <LoadingIcon />;
+        }
+        if (groupCompleted) {
+            return (
+                <LoadMoreCard
+                    getContributions={this.getContributions}
+                    group={group}
+                    navigation={navigation}
+                    projectId={this.project.projectId}
+                    toNextGroup={this.toNextGroup}
+                />
+            );
+        }
+        return (
+            <View style={styles.mappingContainer}>
+                <Header
+                    lookFor={this.project.lookFor}
+                    onBackPress={this.returnToView}
+                />
+                <Component
+                    commitCompletedGroup={this.commitCompletedGroup}
+                    group={group}
+                    project={this.project}
+                    submitFootprintResult={this.submitResult}
+                    updateProgress={this.updateProgress}
+                />
+                <BottomProgress ref={(r) => { this.progress = r; }} />
+            </View>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) => {
+    // if we're offline, there might be more than 1 group in the local
+    // firebase data, for now, we just pick the first one
+    const { projectId } = ownProps.navigation.getParam('project', null);
+    let groupId = '';
+    const { groups } = state.firebase.data.projects[projectId];
+    if (isLoaded(groups)) {
+        // eslint-disable-next-line prefer-destructuring
+        groupId = Object.keys(groups)[0];
+    }
+    return {
+        group: get(state.firebase.data, `projects.${projectId}.groups.${groupId}`),
+        navigation: ownProps.navigation,
+        results: state.results,
+    };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => (
+    {
+        onCancelGroup(groupDetails) {
+            dispatch(cancelGroup(groupDetails));
+        },
+        onCommitGroup(groupInfo) {
+            dispatch(commitGroup(groupInfo));
+        },
+        onStartGroup(groupDetails) {
+            dispatch(startGroup(groupDetails));
+        },
+        onSubmitResult(resultObject) {
+            dispatch(ownProps.submitResultFunction(resultObject));
+        },
+    }
+);
+
+export default compose(
+    firebaseConnect((props) => {
+        const { projectId } = props.navigation.getParam('project', null);
+        if (projectId) {
+            return [
+                {
+                    type: 'once',
+                    path: `groups/${projectId}`,
+                    queryParams: ['limitToLast=1', 'orderByChild=requiredCount'],
+                    storeAs: `projects/${projectId}/groups`,
+                },
+            ];
+        }
+        return [];
+    }),
+    connect(
+        mapStateToProps,
+        mapDispatchToProps,
+    ),
+)(ProjectLevelScreen);

src/shared/views/BuildingFootprint/index.js
@@ -1,234 +1,24 @@
 // @flow
 import * as React from 'react';
+import ProjectLevelScreen from '../../common/ProjectLevelScreen';
 import {
-    BackHandler,
-    StyleSheet,
-    View,
-} from 'react-native';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { firebaseConnect, isEmpty, isLoaded } from 'react-redux-firebase';
-import { get } from 'lodash';
-import {
-    cancelGroup,
-    commitGroup,
-    startGroup,
     submitFootprint,
 } from '../../actions/index';
-import Header from '../Header';
 import Validator from './Validator';
-import BottomProgress from '../Mapper/BottomProgress';
-import LoadingIcon from '../LoadingIcon';
-import LoadMoreCard from '../LoadMore';
-import { getSqKmForZoomLevelPerTile } from '../../Database';
 import type {
-    BuildingFootprintGroupType,
     NavigationProp,
-    ProjectType,
 } from '../../flow-types';
-import {
-    COLOR_DEEP_BLUE,
-} from '../../constants';
-
-const GLOBAL = require('../../Globals');
-
-/* eslint-disable global-require */
-
-const styles = StyleSheet.create({
-    mappingContainer: {
-        backgroundColor: COLOR_DEEP_BLUE,
-        height: GLOBAL.SCREEN_HEIGHT,
-        width: GLOBAL.SCREEN_WIDTH,
-    },
-});
 
 type Props = {
-    group: { [group_id: string]: BuildingFootprintGroupType },
     navigation: NavigationProp,
-    onCancelGroup: {} => void,
-    onStartGroup: {} => void,
-    onSubmitFootprint: (Object) => void,
 };
 
-type State = {
-    groupCompleted: bool,
-};
-
-class BuildingFootprintValidator extends React.Component<Props, State> {
-    constructor(props) {
-        super(props);
-        this.project = props.navigation.getParam('project');
-        this.state = {
-            groupCompleted: false,
-        };
-    }
-
-    componentDidMount() {
-        BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
-    }
-
-    componentDidUpdate = (prevProps) => {
-        const { group, onStartGroup } = this.props;
-        if (prevProps.group !== group) {
-            if (isLoaded(group) && !isEmpty(group)) {
-                // the component props are updated when group is received
-                // and then when tasks are received
-                if (group.tasks !== undefined) {
-                    onStartGroup({
-                        groupId: group.groupId,
-                        projectId: group.projectId,
-                        timestamp: GLOBAL.DB.getTimestamp(),
-                    });
-                    this.setState({ groupCompleted: false });
-                    if (this.progress) this.progress.updateProgress(0);
-                }
-            }
-        }
-    }
-
-    componentWillUnmount() {
-        BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
-    }
-
-    handleBackPress = () => {
-        this.returnToView();
-        return true;
-    }
-
-    returnToView = () => {
-        const { group, navigation, onCancelGroup } = this.props;
-        // TODO: this will not work with offline preloading of multiple groups
-        // as several groups will be stored in redux, possibly with clashing groupId
-        onCancelGroup({
-            groupId: group.groupId,
-            projectId: group.projectId,
-        });
-        navigation.pop();
-    }
-
-    submitFootprintResult = (result, taskId) => {
-        const { group, onSubmitFootprint } = this.props;
-        const resultObject = {
-            resultId: taskId,
-            result,
-            groupId: group.groupId,
-            projectId: this.project.projectId,
-        };
-        onSubmitFootprint(resultObject);
-    }
-
-    commitCompletedGroup = () => {
-        this.setState({ groupCompleted: true });
-    }
-
-    getContributions = (group, results) => {
-        const contributionsCount = Object.keys(results).length;
-        const addedDistance = group.count * getSqKmForZoomLevelPerTile(19);
-        return { contributionsCount, addedDistance };
-    }
-
-    toNextGroup = () => {
-        const { navigation } = this.props;
-        navigation.navigate('BuildingFootprintValidator', { project: this.project });
-    }
-
-    updateProgress = (progress: number) => {
-        if (this.progress) {
-            this.progress.updateProgress(progress);
-        }
-    }
-
-    progress: ?BottomProgress;
-
-    project: ProjectType;
-
-    render = () => {
-        const { group, navigation } = this.props;
-        const { groupCompleted } = this.state;
-        if (!group) {
-            return <LoadingIcon />;
-        }
-        if (groupCompleted) {
-            return (
-                <LoadMoreCard
-                    getContributions={this.getContributions}
-                    group={group}
-                    navigation={navigation}
-                    projectId={this.project.projectId}
-                    toNextGroup={this.toNextGroup}
-                />
-            );
-        }
-        return (
-            <View style={styles.mappingContainer}>
-                <Header
-                    lookFor={this.project.lookFor}
-                    onBackPress={this.returnToView}
-                />
-                <Validator
-                    commitCompletedGroup={this.commitCompletedGroup}
-                    group={group}
-                    project={this.project}
-                    submitFootprintResult={this.submitFootprintResult}
-                    updateProgress={this.updateProgress}
-                />
-                <BottomProgress ref={(r) => { this.progress = r; }} />
-            </View>
-        );
-    }
-}
-
-const mapStateToProps = (state, ownProps) => {
-    // if we're offline, there might be more than 1 group in the local
-    // firebase data, for now, we just pick the first one
-    const { projectId } = ownProps.navigation.getParam('project', null);
-    let groupId = '';
-    const { groups } = state.firebase.data.projects[projectId];
-    if (isLoaded(groups)) {
-        // eslint-disable-next-line prefer-destructuring
-        groupId = Object.keys(groups)[0];
-    }
-    return {
-        group: get(state.firebase.data, `projects.${projectId}.groups.${groupId}`),
-        navigation: ownProps.navigation,
-        results: state.results,
-    };
-};
-
-const mapDispatchToProps = dispatch => (
-    {
-        onCancelGroup(groupDetails) {
-            dispatch(cancelGroup(groupDetails));
-        },
-        onCommitGroup(groupInfo) {
-            dispatch(commitGroup(groupInfo));
-        },
-        onStartGroup(groupDetails) {
-            dispatch(startGroup(groupDetails));
-        },
-        onSubmitFootprint(resultObject) {
-            dispatch(submitFootprint(resultObject));
-        },
-    }
+/* eslint-disable react/destructuring-assignment */
+export default (props: Props) => (
+    <ProjectLevelScreen
+        Component={Validator}
+        navigation={props.navigation}
+        screenName="BuildingFootprintValidator"
+        submitResultFunction={submitFootprint}
+    />
 );
-
-export default compose(
-    firebaseConnect((props) => {
-        const { projectId } = props.navigation.getParam('project', null);
-        if (projectId) {
-            return [
-                {
-                    type: 'once',
-                    path: `groups/${projectId}`,
-                    queryParams: ['limitToLast=1', 'orderByChild=requiredCount'],
-                    storeAs: `projects/${projectId}/groups`,
-                },
-            ];
-        }
-        return [];
-    }),
-    connect(
-        mapStateToProps,
-        mapDispatchToProps,
-    ),
-)(BuildingFootprintValidator);

src/shared/views/ChangeDetection/index.js
@@ -1,236 +1,24 @@
 // @flow
 import * as React from 'react';
+import ProjectLevelScreen from '../../common/ProjectLevelScreen';
 import {
-    BackHandler,
-    StyleSheet,
-    View,
-} from 'react-native';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
-import { firebaseConnect, isEmpty, isLoaded } from 'react-redux-firebase';
-import { get } from 'lodash';
-import {
-    cancelGroup,
-    commitGroup,
-    startGroup,
     submitChange,
 } from '../../actions/index';
-import Header from '../Header';
-import BottomProgress from '../Mapper/BottomProgress';
 import ChangeDetector from './ChangeDetector';
-import LoadingIcon from '../LoadingIcon';
-import LoadMoreCard from '../LoadMore';
-import { getSqKmForZoomLevelPerTile } from '../../Database';
 import type {
-    GroupMapType,
     NavigationProp,
-    ProjectType,
 } from '../../flow-types';
 
-import {
-    COLOR_DEEP_BLUE,
-} from '../../constants';
-
-const GLOBAL = require('../../Globals');
-
-/* eslint-disable global-require */
-
-const styles = StyleSheet.create({
-    mappingContainer: {
-        backgroundColor: COLOR_DEEP_BLUE,
-        height: GLOBAL.SCREEN_HEIGHT,
-        width: GLOBAL.SCREEN_WIDTH,
-    },
-});
-
 type Props = {
-    group: GroupMapType,
     navigation: NavigationProp,
-    onCancelGroup: {} => void,
-    onStartGroup: {} => void,
-    onSubmitChange: (Object) => void,
 };
 
-type State = {
-    groupCompleted: bool,
-};
-
-class _ChangeDetectionScreen extends React.Component<Props, State> {
-    constructor(props) {
-        super(props);
-        this.project = props.navigation.getParam('project');
-        this.state = {
-            groupCompleted: false,
-        };
-    }
-
-    componentDidMount() {
-        BackHandler.addEventListener('hardwareBackPress', this.handleBackPress);
-    }
-
-    componentDidUpdate = (prevProps) => {
-        const { group, onStartGroup } = this.props;
-        if (prevProps.group !== group) {
-            if (isLoaded(group) && !isEmpty(group)) {
-                // the component props are updated twice: when group is received
-                // and then when tasks are received
-                if (group.tasks !== undefined) {
-                    onStartGroup({
-                        groupId: group.groupId,
-                        projectId: group.projectId,
-                        timestamp: GLOBAL.DB.getTimestamp(),
-                    });
-                    this.setState({ groupCompleted: false });
-                    if (this.progress) this.progress.updateProgress(0);
-                }
-            }
-        }
-    }
-
-    componentWillUnmount() {
-        BackHandler.removeEventListener('hardwareBackPress', this.handleBackPress);
-    }
-
-    handleBackPress = () => {
-        this.returnToView();
-        return true;
-    }
-
-    returnToView = () => {
-        const { group, navigation, onCancelGroup } = this.props;
-        // TODO: this will not work with offline preloading of multiple groups
-        // as several groups will be stored in redux, possibly with clashing groupId
-        onCancelGroup({
-            groupId: group.groupId,
-            projectId: group.projectId,
-        });
-        navigation.pop();
-    }
-
-    submitChangeResult = (result, taskId) => {
-        const { group, onSubmitChange } = this.props;
-        const resultObject = {
-            resultId: taskId,
-            result,
-            groupId: group.groupId,
-            projectId: this.project.projectId,
-            timestamp: GLOBAL.DB.getTimestamp(),
-        };
-        onSubmitChange(resultObject);
-    }
-
-    commitCompletedGroup = () => {
-        this.setState({ groupCompleted: true });
-    }
-
-    getContributions = (group, results) => {
-        const contributionsCount = Object.keys(results).length;
-        const addedDistance = group.count * getSqKmForZoomLevelPerTile(19);
-        return { contributionsCount, addedDistance };
-    }
-
-    toNextGroup = () => {
-        const { navigation } = this.props;
-        navigation.navigate('_ChangeDetectionScreen', { project: this.project });
-    }
-
-    updateProgress = (progress: number) => {
-        if (this.progress) {
-            this.progress.updateProgress(progress);
-        }
-    }
-
-    progress: ?BottomProgress;
-
-    project: ProjectType;
-
-    render = () => {
-        const { group, navigation } = this.props;
-        const { groupCompleted } = this.state;
-        if (!group) {
-            return <LoadingIcon />;
-        }
-        if (groupCompleted) {
-            return (
-                <LoadMoreCard
-                    getContributions={this.getContributions}
-                    group={group}
-                    navigation={navigation}
-                    projectId={this.project.projectId}
-                    toNextGroup={this.toNextGroup}
-                />
-            );
-        }
-        return (
-            <View style={styles.mappingContainer}>
-                <Header
-                    lookFor={this.project.lookFor}
-                    onBackPress={this.returnToView}
-                />
-                <ChangeDetector
-                    commitCompletedGroup={this.commitCompletedGroup}
-                    group={group}
-                    project={this.project}
-                    submitChangeResult={this.submitChangeResult}
-                    updateProgress={this.updateProgress}
-                />
-                <BottomProgress ref={(r) => { this.progress = r; }} />
-            </View>
-        );
-    }
-}
-
-const mapStateToProps = (state, ownProps) => {
-    // if we're offline, there might be more than 1 group in the local
-    // firebase data, for now, we just pick the first one
-    const { projectId } = ownProps.navigation.getParam('project', null);
-    let groupId = '';
-    const { groups } = state.firebase.data.projects[projectId];
-    if (isLoaded(groups)) {
-        // eslint-disable-next-line prefer-destructuring
-        groupId = Object.keys(groups)[0];
-    }
-    return {
-        group: get(state.firebase.data, `projects.${projectId}.groups.${groupId}`),
-        navigation: ownProps.navigation,
-        results: state.results,
-    };
-};
-
-const mapDispatchToProps = dispatch => (
-    {
-        onCancelGroup(groupDetails) {
-            dispatch(cancelGroup(groupDetails));
-        },
-        onCommitGroup(groupInfo) {
-            dispatch(commitGroup(groupInfo));
-        },
-        onStartGroup(groupDetails) {
-            dispatch(startGroup(groupDetails));
-        },
-        onSubmitChange(resultObject) {
-            dispatch(submitChange(resultObject));
-        },
-    }
+/* eslint-disable react/destructuring-assignment */
+export default (props: Props) => (
+    <ProjectLevelScreen
+        Component={ChangeDetector}
+        navigation={props.navigation}
+        screenName="_ChangeDetectionScreen"
+        submitResultFunction={submitChange}
+    />
 );
-
-export default compose(
-    firebaseConnect((props) => {
-        const { projectId } = props.navigation.getParam('project', null);
-        if (projectId) {
-            return [
-                {
-                    type: 'once',
-                    path: `groups/${projectId}`,
-                    queryParams: ['limitToLast=1', 'orderByChild=requiredCount'],
-                    storeAs: `projects/${projectId}/groups`,
-                },
-            ];
-        }
-        return [];
-    }),
-    connect(
-        mapStateToProps,
-        mapDispatchToProps,
-    ),
-)(_ChangeDetectionScreen);

commit 07b5f8ec0da7ab9eedf64cbd3763c6911a07ff66

Fix crash when submitting response to a CD or footprint task
This bug was caused by bfbb4c584b19ee7ffe33

src/shared/common/ProjectLevelScreen.js
@@ -171,7 +171,7 @@ class ProjectLevelScreen extends React.Component<Props, State> {
                     commitCompletedGroup={this.commitCompletedGroup}
                     group={group}
                     project={this.project}
-                    submitFootprintResult={this.submitResult}
+                    submitResult={this.submitResult}
                     updateProgress={this.updateProgress}
                 />
                 <BottomProgress ref={(r) => { this.progress = r; }} />

src/shared/views/BuildingFootprint/Validator.js
@@ -39,7 +39,7 @@ type Props = {
     commitCompletedGroup: () => void,
     group: BuildingFootprintGroupType,
     project: ProjectType,
-    submitFootprintResult: (number, string) => void,
+    submitResult: (number, string) => void,
     updateProgress: (number) => void,
 };
 
@@ -85,11 +85,11 @@ class _Validator extends React.Component<Props, State> {
         const {
             commitCompletedGroup,
             group,
-            submitFootprintResult,
+            submitResult,
             updateProgress,
         } = this.props;
         const { currentTaskId } = this.state;
-        submitFootprintResult(result, currentTaskId);
+        submitResult(result, currentTaskId);
         const { done, value } = this.taskGen.next();
         if (done) {
             // no more tasks in the group, commit results and go back to menu
@@ -169,7 +169,7 @@ const mapStateToProps = (state, ownProps) => (
         commitCompletedGroup: ownProps.commitCompletedGroup,
         group: ownProps.group,
         project: ownProps.project,
-        submitFootprintResult: ownProps.submitFootprintResult,
+        submitResult: ownProps.submitResult,
     }
 );
 

src/shared/views/ChangeDetection/ChangeDetector.js
@@ -114,7 +114,7 @@ const minOpacity = 0;
 type Props = {
     commitCompletedGroup: () => void,
     group: ChangeDetectionGroupType,
-    submitChangeResult: (number, string) => void,
+    submitResult: (number, string) => void,
     updateProgress: (number) => void,
 };
 
@@ -266,11 +266,11 @@ class _ChangeDetector extends React.Component<Props, State> {
         const {
             commitCompletedGroup,
             group,
-            submitChangeResult,
+            submitResult,
             updateProgress,
         } = this.props;
         const { currentTaskId } = this.state;
-        submitChangeResult(result, currentTaskId);
+        submitResult(result, currentTaskId);
         const { done, value } = this.taskGen.next();
         if (done) {
             // no more tasks in the group, commit results and go back to menu
@@ -371,7 +371,7 @@ const mapStateToProps = (state, ownProps) => (
         commitCompletedGroup: ownProps.commitCompletedGroup,
         group: ownProps.group,
         project: ownProps.project,
-        submitChangeResult: ownProps.submitChangeResult,
+        submitResult: ownProps.submitResult,
     }
 );
 