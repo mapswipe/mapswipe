371bd8f27a9d633051f49817e62f29af1a52727d
HHK1 <henryhuck@hotmail.fr>
2019-07-24 14:15:51 +0200

    1.3.34(2)-beta

--- scripts/version.sh
+++ scripts/version.sh
@@ -17,5 +17,15 @@ echo "Release version in beta or Prod ?"
     esac
   done
 
+yarn version --new-version $versionNumber --no-git-tag-version
+
+bundle exec fastlane run increment_build_number build_number:$buildNumber
+bundle exec fastlane run increment_version_number version_number:$versionNumber
+
+git commit -a -m $tag
+git tag $tag
+
+git push --follow-tags
+git push --set-upstream origin HEAD
 
 echo $tag
