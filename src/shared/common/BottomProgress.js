6d2ca59550acc0ea97c8a3889f81371de5f57975
Laurent Savaete <laurent@where.tf>
2019-08-28 16:25:38 +0100

    #102 Remove text from progress bar

--- src/shared/common/BottomProgress.js
+++ src/shared/common/BottomProgress.js
@@ -3,7 +3,6 @@ import * as React from 'react';
 import * as Progress from 'react-native-progress';
 import {
     StyleSheet,
-    Text,
     View,
 } from 'react-native';
 import {
@@ -14,15 +13,6 @@ import {
 const GLOBAL = require('../Globals');
 
 const styles = StyleSheet.create({
-    progressBarText: {
-        color: COLOR_LIGHT_GRAY,
-        borderColor: '#212121',
-        fontWeight: '500',
-        position: 'absolute',
-        top: 1,
-        left: GLOBAL.SCREEN_WIDTH - 160,
-        backgroundColor: 'transparent',
-    },
     swipeNavBottom: {
         width: (GLOBAL.SCREEN_WIDTH),
         bottom: 3,
@@ -36,7 +26,6 @@ const styles = StyleSheet.create({
 
 type BPState = {
     progress: number,
-    text: string,
 };
 
 export default class BottomProgress extends React.Component<{}, BPState> {
@@ -44,19 +33,17 @@ export default class BottomProgress extends React.Component<{}, BPState> {
         super(props);
         this.state = {
             progress: 0,
-            text: 'START MAPPING',
         };
     }
 
     updateProgress = (progress: number) => {
         this.setState({
             progress,
-            text: `YOU'VE MAPPED ${Math.ceil(progress * 100)}%`,
         });
     }
 
     render() {
-        const { progress, text } = this.state;
+        const { progress } = this.state;
         return (
             <View style={styles.swipeNavBottom}>
                 <Progress.Bar
@@ -65,10 +52,9 @@ export default class BottomProgress extends React.Component<{}, BPState> {
                     width={GLOBAL.SCREEN_WIDTH * 0.98}
                     marginBottom={2}
                     borderRadius={0}
-                    unfilledColor="#ffffff"
+                    unfilledColor={COLOR_LIGHT_GRAY}
                     progress={progress}
                 />
-                <Text style={styles.progressBarText}>{text}</Text>
             </View>
         );
     }
