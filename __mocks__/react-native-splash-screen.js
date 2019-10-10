47e01a4242fb671cd569ab035fdebf26d83709c5
Laurent Savaete <laurent@where.tf>
2019-10-10 13:17:47 +0100

    Fix tests broken by the splash screen

---
+++ __mocks__/react-native-splash-screen.js
@@ -0,0 +1,4 @@
+export default {
+  show: jest.fn().mockImplementation( () => { console.log('show splash screen'); } ),
+  hide: jest.fn().mockImplementation( () => { console.log('hide splash screen'); } ),
+}
