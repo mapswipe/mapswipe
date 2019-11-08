9f62d21555ef7137ad468429734d9ad6ce2d4c6f
Laurent Savaete <laurent@where.tf>
2019-11-08 11:40:45 +0000

    #100 Link rn-linear-gradient

--- android/app/src/main/java/org/missingmaps/mapswipe/MainApplication.java
+++ android/app/src/main/java/org/missingmaps/mapswipe/MainApplication.java
@@ -3,6 +3,7 @@ package org.missingmaps.mapswipe;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import io.sentry.RNSentryPackage;
 import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
@@ -32,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new AsyncStoragePackage(),
             new RNSentryPackage(),
             new RNCViewPagerPackage(),
