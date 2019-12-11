8e4e95afe0215063f696859620cd0768c51aed52
Laurent Savaete <laurent@where.tf>
2019-12-11 10:23:47 +0000

    #141 Fix error in level calculation that broke progress display

--- __tests__/common/Database.js
+++ __tests__/common/Database.js
@@ -1,4 +1,5 @@
 import { getProjectProgressForDisplay } from '../../src/shared/Database';
+import { getLevelForContributionCount } from '../../src/shared/reducers/ui';
 
 describe('Test various utility functions', () => {
     test('Check that displayed progress is rounded as expected', () => {
@@ -11,6 +12,19 @@ describe('Test various utility functions', () => {
         checks.forEach((check) => {
             const p1 = getProjectProgressForDisplay(check.in);
             expect(p1).toEqual(check.out);
+        });
+    });
+
+    test('Test getLevelForContributionCount', () => {
+        const checks = [
+            { in: 10695, out: 6 },
+            { in: 10694, out: 5 },
+            { in: 1, out: 1 },
+            { in: 1999999999, out: 36 },
+        ];
+        checks.forEach((check) => {
+            const level = getLevelForContributionCount(check.in);
+            expect(level).toEqual(check.out);
         });
     });
 });
