0d79ed8274532412ab94420de7686e5e991f6c5a
frozenhelium <ankit.mehta@togglecorp.com>
2022-11-01 19:16:02 +0545

    Update API endpoints to point to dev instance

--- src/shared/views/UserGroup.js
+++ src/shared/views/UserGroup.js
@@ -553,9 +553,7 @@ function UserGroup(props: Props) {
                         )}
                         <View style={styles.cachedInfo}>
                             <Text style={styles.infoText}>
-                                {t(
-                                    'All the stats are only updated once everyday',
-                                )}
+                                {t('All the stats are only updated once a day')}
                             </Text>
                         </View>
                         <View style={styles.userGroupsStatsContainer}>
