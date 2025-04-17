948b9d87a9357a8a1b08dc107fc1ae0cf04d153e
shreeyash shrestha <shreeyash.shrestha@togglecorp.com>
2025-04-17 11:07:41 +0545

    Update podfile

--- ios/mapswipe/AppDelegate.swift
+++ ios/mapswipe/AppDelegate.swift
@@ -1,30 +1,53 @@
 import UIKit
 import React
+import Firebase
+import RNBootSplash
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 
 @main
 class AppDelegate: RCTAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    self.moduleName = "mapswipe"
-    self.dependencyProvider = RCTAppDependencyProvider()
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        self.moduleName = "mapswipe"
+        self.dependencyProvider = RCTAppDependencyProvider()
 
-    // You can add your custom initial props in the dictionary below.
-    // They will be passed down to the ViewController used by React Native.
-    self.initialProps = [:]
+        // You can add your custom initial props in the dictionary below.
+        // They will be passed down to the ViewController used by React Native.
+        self.initialProps = [:]
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+        if #available(iOS 13.0, *) {
+            rootView.backgroundColor = UIColor.systemBackground
+        } else {
+            rootView.backgroundColor = UIColor.white
+        }
 
-  override func sourceURL(for bridge: RCTBridge) -> URL? {
-    self.bundleURL()
-  }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 
-  override func bundleURL() -> URL? {
-#if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
-#else
-    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
-#endif
-  }
+    override func onReactInstanceReady(_ bridge: RCTBridge!) {
+        super.onReactInstanceReady(bridge)
+        if let rootView = self.window?.rootViewController?.view as? RCTRootView {
+            RNBootSplash.initWithStoryboardName("BootSplash", rootView: rootView)
+        }
+    }
+
+    // deeplink support
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return RCTLinkingManager.application(app, open: url, options: options)
+    }
+
+    override func sourceURL(for bridge: RCTBridge) -> URL? {
+        self.bundleURL()
+    }
+
+    override func bundleURL() -> URL? {
+        #if DEBUG
+        RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+        #else
+        Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+        #endif
+    }
 }
